3bd64894791f7521294a28ba04f9fc1034a0c593
ShadowDragon <kovacsgabor55@gmail.com>
2020-10-30 12:33:23 +0100

    Zeneszámok

---
+++ src/main/java/classstructureattributes/Music.java
@@ -0,0 +1,18 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class Music {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Song song = new Song();
+        System.out.println("Előadó?");
+        song.band = scanner.nextLine();
+        System.out.println("Cím?");
+        song.title = scanner.nextLine();
+        System.out.println("Hossz?");
+        song.length = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(song.band + " - " + song.title + " (" + song.length + " perc)");
+    }
+}
