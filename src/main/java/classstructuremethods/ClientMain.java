98834ab4c1cd8b113c88f5e1529b024679990648
ShadowDragon <kovacsgabor55@gmail.com>
2020-10-30 12:47:41 +0100

    Ügyfél osztály

---
+++ src/main/java/classstructuremethods/ClientMain.java
@@ -0,0 +1,23 @@
+package classstructuremethods;
+
+import java.util.Scanner;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Client client = new Client();
+
+        System.out.println("Név?");
+        client.setName(scanner.nextLine());
+        System.out.println("Év?");
+        client.setYear(scanner.nextInt());
+        client.setAddress(scanner.nextLine());
+        System.out.println("Cím?");
+        client.setAddress(scanner.nextLine());
+        System.out.println("Név: " + client.getName() + " Év: " + client.getYear() + " Cím: " + client.getAddress());
+
+        System.out.println("Új cím?");
+        client.migrate(scanner.nextLine());
+        System.out.println("Új cím: " + client.getName());
+    }
+}
