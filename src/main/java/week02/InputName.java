deaeaeafe9d5c918da10b994d9bfc28899a9bdb1
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-05 14:18:34 +0100

    órai 201105

---
+++ src/main/java/week02/InputName.java
@@ -0,0 +1,19 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputName {
+    public static void main(String[] args) {
+        String[] nevek;
+        Scanner sc = new Scanner(System.in);
+        nevek = new String[5];
+        System.out.println("kérek 5 nevet enterrel elválasztva:");
+        for (int i = 0; i < nevek.length; i++) {
+            nevek[i] = sc.nextLine();
+        }
+
+        for (int i = 0; i < nevek.length; i++) {
+            System.out.println("elem: " + (i+1) + " tartalom: " + nevek[i].toString());
+        }
+    }
+}
