ba284e68b26322869aa2c3b9ca40ac9948d2bdd3
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-06 12:56:13 +0100

    Időpontok még nincs kész.

---
+++ src/main/java/statements/Time.java
@@ -0,0 +1,30 @@
+package statements;
+
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getInMinutes() {
+        return hour * 60 + minute;
+    }
+
+    public int getInSeconds() {
+        return getInMinutes() * 60 + second;
+    }
+
+    public boolean earlierThan(Time other) {
+        return (getInSeconds() < other.getInSeconds());
+    }
+
+    @Override
+    public String toString() {
+        return hour + ":" + minute + ":" + second;
+    }
+}
