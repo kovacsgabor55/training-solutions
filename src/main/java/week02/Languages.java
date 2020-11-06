90e3965239fbd96a2c865e4e92b79a02b5401463
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-06 13:12:06 +0100

    201006 felad

---
+++ src/main/java/week02/Languages.java
@@ -0,0 +1,20 @@
+package week02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+    public static void main(String[] args) {
+        List<String> languages=new ArrayList<>();
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("JavaScript");
+
+        for (String item: languages
+             ) {
+            if(item.length()>5){
+                System.out.println(item.toString());
+            }
+        }
+    }
+}
