85b2e29f4a5e922bb2ed53d7af20dafa4390c0af
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-13 10:49:43 +0100

    Csónakok

---
+++ src/main/java/introcontrol/BoatRental.java
@@ -0,0 +1,41 @@
+package introcontrol;
+
+import java.util.Scanner;
+
+public class BoatRental {
+    //5 3 2
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hány fős csoport érkezett?");
+        int numberOfPeople = scanner.nextInt();
+
+        int freePlaces = 10;
+        int freeBoats = 3;
+
+        if (numberOfPeople > 3) {
+            System.out.println("5-ös csónak");
+            numberOfPeople = numberOfPeople - 5;
+            freeBoats--;
+            freePlaces = freePlaces - 5;
+        }
+        if (numberOfPeople > 2) {
+            System.out.println("3-as csónak");
+            numberOfPeople = numberOfPeople - 3;
+            freeBoats--;
+            freePlaces = freePlaces - 3;
+        }
+        if (numberOfPeople > 0) {
+            System.out.println("2-es csónak");
+            numberOfPeople = numberOfPeople - 2;
+            freeBoats--;
+            freePlaces = freePlaces - 2;
+        }
+        if (numberOfPeople > 0) {
+            System.out.println("Sajnos nem fért el mindenki a hajókban.");
+        } else {
+            System.out.println("Üres helyek száma: " + freePlaces);
+            System.out.println("Üres csónakok száma: " + freeBoats);
+        }
+    }
+}
