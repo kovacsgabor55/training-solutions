0da8956c702faeb79d69de72f189081fc0d17b2e
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-14 12:41:22 +0100

    sameTempValues

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -12,4 +12,14 @@ public class ArraysMain {
     public List<String> daysOfWeek() {
         return Arrays.asList("Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap");
     }
+
+    public String multiplicationTableAsString(int size) {
+        int[][] multiplicationTable = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                multiplicationTable[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return Arrays.deepToString(multiplicationTable);
+    }
 }
