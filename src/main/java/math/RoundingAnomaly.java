86eb29c27e94a14596597dae5d846e61816fae49
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-16 13:16:10 +0100

    majd folytatás

---
+++ src/main/java/math/RoundingAnomaly.java
@@ -0,0 +1,16 @@
+package math;
+
+public class RoundingAnomaly {
+   /*public double[] randomNumbers(int size, double max, int scale){
+
+    }
+    public double roundAfterSum(double[] numbers){
+
+    }
+    public double sumAfterRound(double[] numbers){
+
+    }
+    public double difference(int size, double max, int scale){
+        
+    }*/
+}
