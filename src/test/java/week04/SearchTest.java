20aa05d7f9962ddb77c4409fa7aed53f9d503510
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-20 16:40:57 +0100

    órai 2020.11.20.

--- src/test/java/week04/SearchTest.java
+++ src/test/java/week04/SearchTest.java
@@ -4,24 +4,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
-/* List<Integer> resiltŰ=   getIndexesOfChar("Himalája",'c');*/
 public class SearchTest {
     private Search target;
 
     @BeforeEach
-    public void  setUP(){
-        target=new Search();
+    public void setUP() {
+        target = new Search();
     }
 
     @Test
-    public void Given(){
-        Assertions.assertEquals(Arrays.asList(1),target.getIndexesOfChar("ketchup",'e'));
+    public void Given() {
+        Assertions.assertEquals(Arrays.asList(1), target.getIndexesOfChar("ketchup", 'e'));
     }
+
     @Test
-    public void Given2(){
-        Assertions.assertEquals(Arrays.asList(1,3),target.getIndexesOfChar("kefe",'e'));
+    public void Given2() {
+        Assertions.assertEquals(Arrays.asList(1, 3), target.getIndexesOfChar("kefe", 'e'));
     }
 }
