2b518c625aae309152037c0efcc3155c6fd061f1
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-20 16:40:16 +0100

    órai 2020.11.20.

--- src/main/java/week04/Search.java
+++ src/main/java/week04/Search.java
@@ -4,26 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Search {
-    public static void main(String[] args) {
-
-
-    }
 
     List<Integer> getIndexesOfChar(String str, char c) {
         List<Integer> result = new ArrayList<>();
-        boolean out = true;
-        int from = 0;
-        while (out) {
-            int a = str.indexOf(c, from);
-            if (a != -1) {
-                result.add(a);
-                from = a;
-            } else {
-                out = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                result.add(i);
             }
-            return result;
         }
         return result;
     }
-
 }
