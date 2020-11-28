62cda83e6e94fe3f2fd2fe54c5bf47c998348574
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-28 13:21:50 +0100

    controlselection

---
+++ src/main/java/controlselection/month/DayInMonth.java
@@ -0,0 +1,30 @@
+package controlselection.month;
+
+import java.time.LocalDate;
+
+public class DayInMonth {
+    int numberOfDays(int year, String month) {
+        //return 0;
+        switch (month.toLowerCase()) {
+            case "Január":
+            case "március":
+            case "május":
+            case "július":
+            case "augusztus":
+            case "október":
+            case "december":
+                return 31;
+            case "április":
+            case "június":
+            case "szeptember":
+            case "november":
+                return 30;
+            case "február":
+                return (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) ? 29 : 28;
+            default:
+                throw new IllegalArgumentException("Invalid month: " + month);
+
+        }
+
+    }
+}
