810ee019b2100bc1bd0aad8b556293f8323c499d
ShadowDragon <kovacsgabor55@gmail.com>
2020-11-30 15:35:07 +0100

    órai 2020.11.30.

---
+++ src/main/java/week06d01/ListSelector.java
@@ -0,0 +1,20 @@
+package week06d01;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ListSelector {
+    public String evenIndexString(List<String> strings) {
+        Objects.requireNonNull(strings, "List cannot be null!");
+        if (strings.isEmpty()) {
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < strings.size(); i += 2) {
+            stringBuilder.append(strings.get(i));
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}
---
+++ src/test/java/week06d01/ListSelectorTest.java
@@ -0,0 +1,50 @@
+package week06d01;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListSelectorTest {
+    ListSelector listSelector = new ListSelector();
+    List<String> oddSizeStringList;
+    List<String> evenSizeStringList;
+
+    @DisplayName("Initialization")
+    @BeforeEach
+    void setUp() {
+        oddSizeStringList = Arrays.asList("first", "second", "third");
+        evenSizeStringList = Arrays.asList("first", "second", "third", "fourth");
+    }
+
+    @DisplayName("odd size list")
+    @Test
+    void oddSizeList() {
+        Assertions.assertEquals("[firstthird]", listSelector.evenIndexString(oddSizeStringList));
+    }
+
+    @DisplayName("even size list")
+    @Test
+    void evenSizeList() {
+        Assertions.assertEquals("[firstthird]", listSelector.evenIndexString(evenSizeStringList));
+    }
+
+    @DisplayName("even size list")
+    @Test
+    void emptyList() {
+        Assertions.assertEquals("", listSelector.evenIndexString(new ArrayList<String>()));
+    }
+
+    @DisplayName("null list")
+    @Test
+    void nullList() {
+        Exception ex = assertThrows(NullPointerException.class, () -> listSelector.evenIndexString(null));
+        assertEquals("List cannot be null!", ex.getMessage());
+    }
+}
