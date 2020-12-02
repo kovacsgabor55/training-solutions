d40f897ae086aa965aa980798e4f1d9cbcba4094
ShadowDragon <kovacsgabor55@gmail.com>
2020-12-02 20:52:11 +0100

    methodargvars.examstats

---
+++ src/main/java/methodargvars/examstats/ExamStats.java
@@ -0,0 +1,35 @@
+package methodargvars.examstats;
+
+public class ExamStats {
+    private int maxPoints;
+
+    public ExamStats(int maxPoints) {
+        this.maxPoints = maxPoints;
+    }
+
+    public int getNumberOfTopGrades(int limitInPercent, int... results) {
+
+        if (results == null || results.length == 0) {
+            throw new IllegalArgumentException("Number of results must not be empty!");
+        }
+        int count = 0;
+        for (int item : results) {
+            if (item > limitInPercent) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean hasAnyFailed(int limitInPercent, int... results) {
+        if (results == null || results.length == 0) {
+            throw new IllegalArgumentException("Number of results must not be empty!");
+        }
+        for (int item : results) {
+            if (item < limitInPercent) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
