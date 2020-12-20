5d9883a39a03c814d4f428249103fc0775dea8bc
ShadowDragon <kovacsgabor55@gmail.com>
2020-12-20 23:55:05 +0100

    temperatures

---
+++ src/main/java/temperatures/TemperaturesReader.java
@@ -0,0 +1,17 @@
+package temperatures;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TemperaturesReader {
+    public Temperatures readTemperatures(String pathString) {
+        Path file = Path.of(pathString);
+        try {
+            return new Temperatures(Files.readAllBytes(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
