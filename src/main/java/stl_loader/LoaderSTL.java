d630ff75c694568bbe4500cf2613863ebd2db0cf
ShadowDragon <kovacsgabor55@gmail.com>
2020-12-22 02:03:13 +0100

    STL loader

--- src/main/java/stl_loader/LoaderSTL.java
+++ src/main/java/stl_loader/LoaderSTL.java
@@ -2,15 +2,16 @@ package stl_loader;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public final class LoaderSTLtxt {
-    private LoaderSTLtxt() {
+public final class LoaderSTL {
+    private LoaderSTL() {
     }
 
-    static Solid loadSTL(Path path) {
+    static Solid loadSTLtxt(Path path) {
         Solid solid = null;
         try (BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset())) {
             String str;
@@ -37,7 +38,31 @@ public final class LoaderSTLtxt {
         return solid;
     }
 
-    public static void main(String[] args) {
-        System.out.println(loadSTL(Path.of("src/main/resources/Sphericon.stl")));
+    static Solid loadSTLbin(Path path) {
+        Solid solid = null;
+        try (InputStream is = Files.newInputStream(path)) {
+
+            String str = new String(is.readNBytes(80));
+            System.out.println(str);
+            byte[] nroftrianglesbin = is.readNBytes(4);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 3; i >= 0; i--) {
+                sb.append(String.format("%8s", Integer.toBinaryString(nroftrianglesbin[i] & 0xFF)).replace(' ', '0'));
+            }
+            int decimal = Integer.parseInt(sb.toString(), 2);
+            for (int i = 0; i <= decimal; i++) {
+                byte[] datein = is.readNBytes(50);
+            }
+            System.out.println(decimal);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return solid;
     }
+
+    public static void main(String[] args) {
+        //System.out.println(loadSTLtxt(Path.of("src/main/resources/Sphericon.stl")));
+        System.out.println(loadSTLbin(Path.of("src/main/resources/Utah_teapot_(solid).stl")));
+    }
+
 }
