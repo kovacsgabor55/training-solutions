a34a4ad92874b27d765a01cc7ab554db906ece0d
ShadowDragon <kovacsgabor55@gmail.com>
2020-12-31 23:44:18 +0100

    quest folyamatban (alakul)

--- src/main/java/timesheet/Company.java
+++ src/main/java/timesheet/Company.java
@@ -52,23 +52,54 @@ public class Company {
     }
 
     public List<ReportLine> calculateProjectByNameYearMonth(String name, int year, Month month) {
-        List<ReportLine> re = new ArrayList<>();
+        List<ReportLine> reportLines = new ArrayList<>();
+        boolean added = false;
         for (TimeSheetItem item : timeSheetItems) {
             if (item.getEmployee().getName().equals(name) && item.getBeginDate().getYear() == year && item.getBeginDate().getMonth() == month) {
-                re.add(new ReportLine(item.getProject(), item.countDifferenceBetweenDates().toHours()));
-                System.out.println(re.size());
+                if (reportLines.isEmpty()) {
+                    reportLines.add(new ReportLine(item.getProject(), item.countDifferenceBetweenDates().toHours()));
+                } else {
+                    for (ReportLine re : reportLines) {
+                        if (re.getProject().getName().equals(item.getProject().getName())) {
+                            re.setTime(item.countDifferenceBetweenDates().toHours());
+                            added = true;
+                        }
+                    }
+                    if (!added) {
+                        reportLines.add(new ReportLine(item.getProject(), item.countDifferenceBetweenDates().toHours()));
+
+                    }
+                    added = false;
+                }
             }
         }
         //TODO
-        return re;
+        return reportLines;
     }
 
     public String prepareReport(String name, int year, Month month) {
         if (validator.notInEmployeesList(employees, name)) {
             throw new IllegalArgumentException();
         }
+        String monthname;
+        if (month.getValue() < 10) {
+            monthname = "0" + month.getValue();
+        } else {
+            monthname = "" + month.getValue();
+        }
+        StringBuilder sb = new StringBuilder(name + "\t" + year + "-" + monthname + "-" + "01\t");
+        long fullHour = 0;
+
+        List<ReportLine> reportLines = calculateProjectByNameYearMonth(name, year, month);
+        for (ReportLine item : reportLines) {
+            fullHour += item.getTime();
+        }
+        sb.append(fullHour);
+        for (ReportLine item : reportLines) {
+            sb.append("\n" + item.getProject().getName() + "\t" + item.getTime());
+        }
         //TODO
-        return null;
+        return sb.toString();
     }
 
     public void printToFile(String name, int year, Month month, Path path) {
