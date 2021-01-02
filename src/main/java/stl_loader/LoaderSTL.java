1a2e044ae33d599b5f7bcdfc8a8c64c294635fbb
ShadowDragon <kovacsgabor55@gmail.com>
2021-01-02 23:45:48 +0100

    stl konverálás próbálkozás (double)

--- src/main/java/stl_loader/LoaderSTL.java
+++ src/main/java/stl_loader/LoaderSTL.java
@@ -1,8 +1,8 @@
 package stl_loader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -40,19 +40,36 @@ public final class LoaderSTL {
 
     static Solid loadSTLbin(Path path) {
         Solid solid = null;
-        try (InputStream is = Files.newInputStream(path)) {
+        try (DataInputStream dis = new DataInputStream(new BufferedInputStream(Files.newInputStream(path)))) {
 
-            String str = new String(is.readNBytes(80));
+            String str = new String(dis.readNBytes(80));
             System.out.println(str);
-            byte[] nroftrianglesbin = is.readNBytes(4);
-            StringBuilder sb = new StringBuilder();
-            for (int i = 3; i >= 0; i--) {
-                sb.append(String.format("%8s", Integer.toBinaryString(nroftrianglesbin[i] & 0xFF)).replace(' ', '0'));
+            int decimal = readReverseInt(dis);
+
+                    //     1234567812345678123456781234567812345678123456781234567812345678
+            String text = "00110011001100110100101111000001";
+            System.out.println(Double.longBitsToDouble(Integer.parseInt(text,2)));
+            System.out.println(Double.longBitsToDouble(new BigInteger(text, 2).longValue()));
+            //double doubleVal = Double.longBitsToDouble(new BigInteger(text, 2).longValue());
+
+            System.out.println("itt");
+            System.out.println(Double.doubleToLongBits(12.5));
+            long ha = Double.doubleToLongBits(12.5);
+            String bin = Long.toBinaryString(ha);
+            System.out.println(bin);
+            System.out.println(Double.longBitsToDouble(new BigInteger(bin, 2).longValue()));
+            System.out.println("itt");
+            //System.out.println(doubleVal);
+
+
+            for (int i = 0; i < 12; i++) {
+                 System.out.println(readReverseDouble(dis));
             }
-            int decimal = Integer.parseInt(sb.toString(), 2);
-            for (int i = 0; i <= decimal; i++) {
-                byte[] datein = is.readNBytes(50);
-            }
+            /*for (int i = 0; i <= decimal; i++) {
+                byte[] datein = dis.readNBytes(50);
+            }*/
+            System.out.println(decimal);
+            //9438
             System.out.println(decimal);
         } catch (IOException e) {
             e.printStackTrace();
@@ -62,7 +79,36 @@ public final class LoaderSTL {
 
     public static void main(String[] args) {
         //System.out.println(loadSTLtxt(Path.of("src/main/resources/Sphericon.stl")));
-        System.out.println(loadSTLbin(Path.of("src/main/resources/Utah_teapot_(solid).stl")));
+        System.out.println(loadSTLbin(Path.of("src/main/resources/teszt.stl")));
     }
 
+    private static double readReverseDouble(DataInputStream dis) throws IOException {
+        byte[] nroftrianglesbin = dis.readNBytes(4);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 3; i >= 0; i--) {
+            sb.append(String.format("%8s", Integer.toBinaryString(nroftrianglesbin[i] & 0xFF)).replace(' ', '0'));
+        }
+        return Double.longBitsToDouble(new BigInteger(sb.toString(), 2).longValue());
+    }
+
+    private static int readReverseInt(DataInputStream dis) throws IOException {
+        byte[] nroftrianglesbin = dis.readNBytes(4);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 3; i >= 0; i--) {
+            sb.append(String.format("%8s", Integer.toBinaryString(nroftrianglesbin[i] & 0xFF)).replace(' ', '0'));
+        }
+        return Integer.parseInt(sb.toString(), 2);
+    }
+
+   /* private static double readReverseDouble(DataInputStream dis) throws IOException{
+        byte[] nroftrianglesbin = dis.readNBytes(4);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 3; i >= 0; i--) {
+            sb.append(String.format("%8s", Integer.toBinaryString(nroftrianglesbin[i] & 0xFF)).replace(' ', '0'));
+        }
+        return Double.parseDouble(sb.toString(), 2);
+    }
+    public static double toDouble(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getDouble();
+    }*/
 }
