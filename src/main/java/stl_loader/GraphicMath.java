54441e11d088af66dc5c3d9a05168c53a646944e
ShadowDragon <kovacsgabor55@gmail.com>
2021-01-18 01:07:42 +0100

    facet normal calculation begin

---
+++ src/main/java/stl_loader/GraphicMath.java
@@ -0,0 +1,8 @@
+package stl_loader;
+
+public class GraphicMath {
+    public static Normal calculateNormalVector(Vertex v1, Vertex v2, Vertex v3) {
+//TODO calculate
+        return null;
+    }
+}
