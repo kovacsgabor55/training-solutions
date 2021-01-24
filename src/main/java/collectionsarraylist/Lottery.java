fa409ac03ca23147d9812e1d20cc59ab87d68b41
ShadowDragon <kovacsgabor55@gmail.com>
2021-01-24 20:48:44 +0100

    collectionsarraylist

---
+++ src/main/java/collectionsarraylist/Lottery.java
@@ -0,0 +1,25 @@
+package collectionsarraylist;
+
+import java.util.*;
+
+public class Lottery {
+    Random random = new Random();
+
+    public List<Integer> selectWinningNumbers(int lotteryType, int ballCount) {
+
+        if (lotteryType >= ballCount) {
+            throw new IllegalArgumentException("Balls must be more then the winning numbers!");
+        }
+        Set<Integer> lottery = new HashSet<>();
+
+        while (lottery.size() < lotteryType) {
+            int i = random.nextInt(ballCount) + 1;
+            lottery.add(i);
+        }
+        List<Integer> res = new ArrayList<>();
+        for (Integer item : lottery) {
+            res.add(item);
+        }
+        return res;
+    }
+}
