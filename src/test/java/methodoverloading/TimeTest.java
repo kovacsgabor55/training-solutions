6bb3039306ab203a1e63679cfbf81608a19a7e56
ShadowDragon <kovacsgabor55@gmail.com>
2021-01-26 17:33:59 +0100

    refactor package methodoverloading

--- src/test/java/methodoverloading/TimeTest.java
+++ src/test/java/methodoverloading/TimeTest.java
@@ -1,4 +1,4 @@
-package methoverloading;
+package methodoverloading;
 
 import org.junit.Test;
 
--- src/test/java/methodoverloading/pub/ListOfGoodPubsTest.java
+++ src/test/java/methodoverloading/pub/ListOfGoodPubsTest.java
@@ -1,4 +1,4 @@
-package methoverloading.pub;
+package methodoverloading.pub;
 
 import org.junit.Rule;
 import org.junit.Test;
