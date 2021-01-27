5210794170d749eec8b2791dc184b0dffe0b3f67
ShadowDragon <kovacsgabor55@gmail.com>
2021-01-27 21:29:05 +0100

    collectionsmap

---
+++ src/main/java/collectionsmap/LogParser.java
@@ -0,0 +1,32 @@
+package collectionsmap;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LogParser {
+    public Map<String, List<Entry>> parseLog(String log) {
+        try {
+
+            Map<String, List<Entry>> ret = new HashMap<>();
+            String[] lines = log.split("\n");
+            List<Entry> entries = new ArrayList<>();
+            for (String item : lines) {
+                String[] data = item.split(":");
+                if (data.length != 3) {
+                    throw new IllegalArgumentException("Incorrect log: incorrect number of fields");
+                }
+                entries.add(new Entry(data[0], LocalDate.parse(data[1]), data[2]));
+                System.out.println(item);
+            }
+            ret.put(entries.get(0).getIpAddress(), entries);
+            return ret;
+        } catch (
+                DateTimeParseException e) {
+            throw new IllegalArgumentException("Incorrect log: incorrect date", e);
+        }
+    }
+}
