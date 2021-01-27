f555ebaf3cc99f618bd1741f1d60861bfd3fece9
ShadowDragon <kovacsgabor55@gmail.com>
2021-01-27 18:16:29 +0100

    week13d01

---
+++ src/main/java/week13d01/Towns.java
@@ -0,0 +1,38 @@
+package week13d01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Towns {
+
+    public String longestTownName(Path path) {
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            return longestName(br);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
+
+    private void skipheader(BufferedReader br) throws IOException {
+        br.readLine();
+    }
+
+    private String longestName(BufferedReader br) throws IOException {
+        int max = 0;
+        String longest = "";
+        skipheader(br);
+        String line;
+        while ((line = br.readLine()) != null) {
+            int index1 = line.indexOf(";");
+            int index2 = line.indexOf(";", index1 + 1);
+            String town = line.substring(index1 + 1, index2);
+            if (town.length() > max) {
+                max = town.length();
+                longest = town;
+            }
+        }
+        return longest;
+    }
+}
---
+++ src/test/java/week13d01/TownsTest.java
@@ -0,0 +1,15 @@
+package week13d01;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+public class TownsTest {
+
+    @Test
+    public void testLongestTownName() {
+        Path path = Path.of("src/main/resources/iranyitoszamok-varosok-2021.csv");
+        Assertions.assertEquals("Jászfelsőszentgyörgy", new Towns().longestTownName(path));
+    }
+}
