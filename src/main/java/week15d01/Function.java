7c2a1d920de6415f7419f3b59d0a1d8c80f53391
ShadowDragon <kovacsgabor55@gmail.com>
2021-02-09 09:33:42 +0100

    week15d01

--- src/main/java/week15d01/Function.java
+++ src/main/java/week15d01/Function.java
@@ -7,15 +7,15 @@ public class Function {
     public Map<Double, Double> functionGlobalMax(Map<Double, Double> input) {
         Map<Double, Double> result = new HashMap<>();
         double functionMaxValue = (Double) input.keySet().toArray()[0];
-        double functionkey = (Double) input.values().toArray()[0];
+        double functionKey = (Double) input.values().toArray()[0];
 
         for (Double item : input.keySet()) {
             if (input.get(item) > functionMaxValue) {
                 functionMaxValue = input.get(item);
-                functionkey = item;
+                functionKey = item;
             }
         }
-        result.put(functionkey, functionMaxValue);
+        result.put(functionKey, functionMaxValue);
         return result;
     }
 
@@ -26,5 +26,18 @@ public class Function {
         in.put(7D, 9D);
         Function fn = new Function();
         System.out.println(fn.functionGlobalMax(in));
+    }
+
+    public Map.Entry<Double, Double> maxValueEntry(Map<Double, Double> graphOfFunction) {
+        if (graphOfFunction.isEmpty() || graphOfFunction.containsValue(Double.MIN_VALUE)) {
+            throw new IllegalArgumentException("Wrong param!");
+        }
+        Map.Entry<Double, Double> maxElement = null;
+        for (Map.Entry<Double, Double> actual : graphOfFunction.entrySet()) {
+            if (maxElement == null || maxElement.getValue() < actual.getValue()) {
+                maxElement = actual;
+            }
+        }
+        return maxElement;
     }
 }
