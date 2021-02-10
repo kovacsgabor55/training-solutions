1b1e72b789497a0239a1f66644c1e365c3fdd3da
ShadowDragon <kovacsgabor55@gmail.com>
2021-02-10 15:55:50 +0100

    week15d03

--- src/main/java/week15d03/PostFinder.java
+++ src/main/java/week15d03/PostFinder.java
@@ -6,15 +6,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PostFinder {
-    private final List<Post> posztmen;
+    private final List<Post> posts;
 
     public PostFinder(List<Post> posztmen) {
-        this.posztmen = new ArrayList<>(posztmen);
+        this.posts = new ArrayList<>(posztmen);
     }
 
     public List<Post> findPosts(String user) {
         List<Post> result = new ArrayList<>();
-        for (Post item : posztmen) {
+        for (Post item : posts) {
             if (item.getOwner().equals(user) && item.getPublishedAt().isBefore(LocalDate.now())) {
                 result.add(item);
             }
@@ -23,14 +23,14 @@ public class PostFinder {
         return result;
     }
 
-    public List<Post> getPosztmen() {
-        return new ArrayList<>(posztmen);
+    public List<Post> getPosts() {
+        return new ArrayList<>(posts);
     }
 
     public static void main(String[] args) {
-        Post ps = new Post("tit", LocalDate.of(2015,02,12), "do", "ow");
+        Post ps = new Post("tit", LocalDate.of(2015, 02, 12), "do", "ow");
         PostFinder postFinder = new PostFinder(Arrays.asList(ps));
-        System.out.println(postFinder.getPosztmen().size());
+        System.out.println(postFinder.getPosts().size());
         System.out.println(postFinder.findPosts("ow"));
     }
 }
