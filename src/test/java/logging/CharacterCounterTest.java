9c3581e3fe18586283bc88f3c0a97d9b6f4eb243
ShadowDragon <kovacsgabor55@gmail.com>
2021-04-21 13:47:21 +0200

    logging

---
+++ src/test/java/logging/CharacterCounterTest.java
@@ -0,0 +1,19 @@
+package logging;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CharacterCounterTest {
+
+    @Test
+    public void testFind() {
+        assertEquals(0,new CharacterCounter().countCharacters("", "abcd"));
+        assertEquals(0,new CharacterCounter().countCharacters("abcd", ""));
+        assertEquals(1,new CharacterCounter().countCharacters("a", "a"));
+        assertEquals(4,new CharacterCounter().countCharacters("aaaa", "a"));
+        assertEquals(4,new CharacterCounter().countCharacters("aaaa", "abcd"));
+        assertEquals(5,new CharacterCounter().countCharacters("abcdabcdabcdabce", "ae"));
+    }
+}
